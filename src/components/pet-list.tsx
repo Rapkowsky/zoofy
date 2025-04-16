5b9c36a922521f9badb8366fbc712f6e33918a1a
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-16 17:19:23 +0200

    Enhance PetList component to display dynamically fetched pet details with images

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,9 +1,24 @@
-export default function PetList() {
+import Image from "next/image";
+
+export default function PetList({ pets }) {
 	return (
 		<ul className="bg-white border-b border-light">
-			<li>
-				<button>Benjamin</button>
-			</li>
+			{pets.map((pet) => {
+				return (
+					<li key={pet.id}>
+						<button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] ">
+							<Image
+								src={pet.imageUrl}
+								alt="Pet image"
+								width={45}
+								height={45}
+								className="w-[45px] h-[45px] rounded-full object-cover"
+							/>
+							<p className="font-semibold">{pet.name}</p>
+						</button>
+					</li>
+				);
+			})}
 		</ul>
 	);
 }
