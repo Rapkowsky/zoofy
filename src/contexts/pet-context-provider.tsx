e1932991dc616ad738a26d4b14ac10272f5c352f
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-16 23:02:32 +0200

    Add selectedPet derived state to PetContextProvider and include it in the context value for better state management.

--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -11,6 +11,7 @@ type TPetContext = {
 	pets: Pet[];
 	selectedPetId: string | null;
 	handleChangeSelectedPetId: (id: string) => void;
+	selectedPet: Pet | undefined;
 };
 
 export const PetContext = createContext<TPetContext | null>(null);
@@ -19,16 +20,22 @@ export default function PetContextProvider({
 	data,
 	children,
 }: PetContextProviderProps) {
+	// States
 	const [pets, setPets] = useState(data);
 	const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
 
+	// Derived states
+
+	const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+
+	// Event handlers / Actions
 	const handleChangeSelectedPetId = (id: string) => {
 		setSelectedPetId(id);
 	};
 
 	return (
 		<PetContext.Provider
-			value={{ pets, selectedPetId, handleChangeSelectedPetId }}>
+			value={{ pets, selectedPetId, handleChangeSelectedPetId, selectedPet }}>
 			{children}
 		</PetContext.Provider>
 	);
