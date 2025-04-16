6c343803a0bf7ccb057bea30867e9576dd21dce7
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-16 19:31:27 +0200

    Add PetContextProvider to manage pet data globally

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,17 +1,30 @@
 import AppFooter from "@/components/app-footer";
 import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
+import PetContextProvider from "@/contexts/pet-context-provider";
+import { Pet } from "@/lib/types";
 import React from "react";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const response = await fetch(
+		"https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+	);
+	if (!response.ok) {
+		throw new Error("Could not fetch pets");
+	}
+	const data: Pet[] = await response.json();
+
 	return (
 		<div>
 			<BackgroundPattern />
 
 			<div className="flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
 				<AppHeader />
-				{children}
-
+				<PetContextProvider data={data}>{children}</PetContextProvider>
 				<AppFooter />
 			</div>
 		</div>
