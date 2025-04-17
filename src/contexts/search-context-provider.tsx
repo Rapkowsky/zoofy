f9b087334f71a6234ceb124be1d1ac7bb2a17656
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-17 15:06:01 +0200

    Add SearchContextProvider and related hook for managing search state.

---
+++ src/contexts/search-context-provider.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { createContext, useState } from "react";
+
+type SearchContextProviderProps = {
+	children: React.ReactNode;
+};
+
+type TSearchContext = {
+	searchQuery: string;
+	handleChangeSearchQuery: (newValue: string) => void;
+};
+
+export const SearchContext = createContext<TSearchContext | null>(null);
+
+export default function SearchContextProvider({
+	children,
+}: SearchContextProviderProps) {
+	// States
+	const [searchQuery, setSearchQuery] = useState("");
+	// Derived states
+
+	// Event handlers / Actions
+	const handleChangeSearchQuery = (newValue: string) => {
+		setSearchQuery(newValue);
+	};
+
+	return (
+		<SearchContext.Provider
+			value={{
+				searchQuery,
+				handleChangeSearchQuery,
+			}}>
+			{children}
+		</SearchContext.Provider>
+	);
+}
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,8 +1,18 @@
 import { PetContext } from "@/contexts/pet-context-provider";
+import { SearchContext } from "@/contexts/search-context-provider";
 import { useContext } from "react";
 
-export default function UsePetContext() {
+export function usePetContext() {
 	const context = useContext(PetContext);
+
+	if (!context) {
+		throw new Error("usePetContext must be used within a PetContextProvider");
+	}
+
+	return context;
+}
+export function useSearchContext() {
+	const context = useContext(SearchContext);
 
 	if (!context) {
 		throw new Error("usePetContext must be used within a PetContextProvider");
