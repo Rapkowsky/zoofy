252cad38252d1e6b69426ae0023944644928323c
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-18 16:41:53 +0200

    Enhance account page layout with structured components

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,5 +1,17 @@
+import ContentBlock from "@/components/content-block";
+import H1 from "@/components/h1";
 import React from "react";
 
 export default function Page() {
-	return <div>AccountPage</div>;
+	return (
+		<main>
+			<H1 className="my-8 text-white">Your account</H1>
+
+			<div className="h-[600px]">
+				<ContentBlock className="h-[500px] flex justify-center items-center">
+					<p>Logged in as...</p>
+				</ContentBlock>
+			</div>
+		</main>
+	);
 }
