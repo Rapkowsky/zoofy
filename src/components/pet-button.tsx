4aab1857020f343f8719bc6f3ae8b950723d7e82
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-23 22:22:57 +0200

    Enhance PetButton component with Dialog integration for add/edit actions

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,6 +1,16 @@
 import { PlusIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 
+import {
+	Dialog,
+	DialogContent,
+
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "./ui/dialog";
+import PetForm from "./pet-form";
+
 type PetButtonProps = {
 	actionType: "add" | "edit" | "checkout";
 	children?: React.ReactNode;
@@ -12,16 +22,6 @@ export default function PetButton({
 	children,
 	onClick,
 }: PetButtonProps) {
-	if (actionType === "add") {
-		return (
-			<Button size="icon">
-				<PlusIcon className="w-6 h-6" />
-			</Button>
-		);
-	}
-	if (actionType === "edit") {
-		return <Button variant="secondary">{children}</Button>;
-	}
 	if (actionType === "checkout") {
 		return (
 			<Button variant="secondary" onClick={onClick}>
@@ -29,4 +29,28 @@ export default function PetButton({
 			</Button>
 		);
 	}
+
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				{actionType === "add" ? (
+					<Button size="icon">
+						<PlusIcon className="w-6 h-6" />
+					</Button>
+				) : (
+					<Button variant="secondary">{children}</Button>
+				)}
+		
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>
+						{" "}
+						{actionType === "add" ? "Add a new pet" : "Edit pet"}
+					</DialogTitle>
+				</DialogHeader>
+				<PetForm />
+			</DialogContent>
+		</Dialog>
+	);
 }
