ca8a022c7ca056b133162376db3581553998da55
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-27 16:57:21 +0200

    - RevalidatePath /app layout file after adding a pet - Adjust state management for pets by directly using the data prop instead of creating a new state variable

--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -22,11 +22,10 @@ type TPetContext = {
 export const PetContext = createContext<TPetContext | null>(null);
 
 export default function PetContextProvider({
-	data,
+	data: pets,
 	children,
 }: PetContextProviderProps) {
 	// States
-	const [pets, setPets] = useState(data);
 	const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
 
 	// Derived states
