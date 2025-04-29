563d25cb0c53887ccb765960b61e18dba9042ca9
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-29 18:33:27 +0200

    Refactor PetContextProvider to implement optimistic UI for Edit pet and Checkout pet

--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -30,14 +30,19 @@ export default function PetContextProvider({
 
 	const [optimisticPets, setOptimisticPets] = useOptimistic(
 		data,
-		(state, newPet) => {
-			return [
-				...state,
-				{
-					...newPet,
-					id: Math.random().toString(),
-				},
-			];
+		(state, { action, payload }) => {
+			switch (action) {
+				case "add":
+					return [...state, { ...payload, id: Math.random().toString() }];
+				case "edit":
+					return state.map((pet) =>
+						pet.id === payload.id ? { ...pet, ...payload.newPetData } : pet
+					);
+				case "delete":
+					return state.filter((pet) => pet.id !== payload);
+				default:
+					return state;
+			}
 		}
 	);
 	const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
@@ -49,7 +54,7 @@ export default function PetContextProvider({
 	// Event handlers / Actions
 
 	const handleAddPet = async (newPet: Omit<Pet, "id">) => {
-		setOptimisticPets(newPet);
+		setOptimisticPets({ action: "add", payload: newPet });
 		const error = await addPet(newPet);
 		if (error) {
 			toast.warning(error.message);
@@ -58,6 +63,7 @@ export default function PetContextProvider({
 	};
 
 	const handleEditPet = async (petId: string, newPetData: Omit<Pet, "id">) => {
+		setOptimisticPets({ action: "edit", payload: { id: petId, newPetData } });
 		const error = await editPet(petId, newPetData);
 		if (error) {
 			toast.warning(error.message);
@@ -66,7 +72,12 @@ export default function PetContextProvider({
 	};
 
 	const handleCheckoutPet = async (petId: string) => {
-		await checkoutPet(petId);
+		setOptimisticPets({ action: "delete", payload: petId });
+		const error = await checkoutPet(petId);
+		if (error) {
+			toast.warning(error.message);
+			return;
+		}
 		setSelectedPetId(null);
 	};
 
