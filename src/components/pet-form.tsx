4d3d3846971c501a88c48074fb9e921f6634d40d
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-04 16:14:47 +0200

    Transform data (imageURL) using Zod + implement getValues RHF function to get the data

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -8,26 +8,32 @@ import PetFormBtn from "./pet-form-btn";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { DEFAULT_PET_IMAGE } from "@/lib/constants";
 
 type PetFormProps = {
 	actionType: "add" | "edit";
 	onFormSubmission: () => void;
 };
 
-const petFormSchema = z.object({
-	name: z.string().trim().min(1, { message: "Name is required" }).max(100),
-	ownerName: z
-		.string()
-		.trim()
-		.min(1, { message: "Owner name is required" })
-		.max(100),
-	imageUrl: z.union([
-		z.literal(""),
-		z.string().trim().url({ message: "Image url must be a valid url" }),
-	]),
-	age: z.coerce.number().int().positive().max(9999),
-	notes: z.union([z.literal(""), z.string().trim().max(1000)]),
-});
+const petFormSchema = z
+	.object({
+		name: z.string().trim().min(1, { message: "Name is required" }).max(100),
+		ownerName: z
+			.string()
+			.trim()
+			.min(1, { message: "Owner name is required" })
+			.max(100),
+		imageUrl: z.union([
+			z.literal(""),
+			z.string().trim().url({ message: "Image url must be a valid url" }),
+		]),
+		age: z.coerce.number().int().positive().max(9999),
+		notes: z.union([z.literal(""), z.string().trim().max(1000)]),
+	})
+	.transform((data) => ({
+		...data,
+		imageUrl: data.imageUrl || DEFAULT_PET_IMAGE,
+	}));
 
 type TPetForm = z.infer<typeof petFormSchema>;
 
@@ -40,6 +46,7 @@ export default function PetForm({
 	const {
 		register,
 		trigger,
+		getValues,
 		formState: { errors },
 	} = useForm<TPetForm>({
 		resolver: zodResolver(petFormSchema),
@@ -53,16 +60,8 @@ export default function PetForm({
 
 				onFormSubmission();
 
-				const petData = {
-					name: formData.get("name") as string,
-					ownerName: formData.get("ownerName") as string,
-					imageUrl:
-						(formData.get("imageUrl") as string) ||
-						"https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
-					age: parseInt(formData.get("age") as string),
-					notes: formData.get("notes") as string,
-				};
-
+				const petData = getValues();
+				petData.imageUrl = petData.imageUrl || DEFAULT_PET_IMAGE;
 				if (actionType === "add") {
 					await handleAddPet(petData);
 				} else if (actionType === "edit") {
