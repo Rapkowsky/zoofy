772458eccf84b250edf33d1b3c414dca6e40914e
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-04 16:40:44 +0200

    Implement Zod schema on the server

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -6,36 +6,10 @@ import { Textarea } from "./ui/textarea";
 import { usePetContext } from "@/lib/hooks";
 import PetFormBtn from "./pet-form-btn";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { DEFAULT_PET_IMAGE } from "@/lib/constants";
-
-type PetFormProps = {
-	actionType: "add" | "edit";
-	onFormSubmission: () => void;
-};
-
-const petFormSchema = z
-	.object({
-		name: z.string().trim().min(1, { message: "Name is required" }).max(100),
-		ownerName: z
-			.string()
-			.trim()
-			.min(1, { message: "Owner name is required" })
-			.max(100),
-		imageUrl: z.union([
-			z.literal(""),
-			z.string().trim().url({ message: "Image url must be a valid url" }),
-		]),
-		age: z.coerce.number().int().positive().max(9999),
-		notes: z.union([z.literal(""), z.string().trim().max(1000)]),
-	})
-	.transform((data) => ({
-		...data,
-		imageUrl: data.imageUrl || DEFAULT_PET_IMAGE,
-	}));
-
-type TPetForm = z.infer<typeof petFormSchema>;
+import { PetFormProps, petFormSchema, TPetForm } from "@/lib/validations";
 
 export default function PetForm({
 	actionType,
@@ -54,7 +28,7 @@ export default function PetForm({
 
 	return (
 		<form
-			action={async (formData) => {
+			action={async () => {
 				const result = await trigger();
 				if (!result) return;
 
