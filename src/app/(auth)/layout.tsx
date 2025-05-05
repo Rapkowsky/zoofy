7a1944f9635fa30c9c6937201afb47665bd0d2de
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-05 12:45:52 +0200

    create common layout for signup and login page + create auth form structure

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/components/logo";
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="flex justify-center items-center min-h-screen flex-col gap-y-5">
+			<Logo />
+			{children}
+		</div>
+	);
+}
