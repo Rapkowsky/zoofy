3527e7052fee613494a9d72f242832c766ec40d9
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-07 10:34:59 +0200

    Implement logOut using server action

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -3,6 +3,7 @@ import H1 from "@/components/h1";
 import React from "react";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import SignOutBtn from "@/components/sign-out-btn";
 
 export default async function Page() {
 	const session = await auth();
@@ -14,8 +15,9 @@ export default async function Page() {
 			<H1 className="my-8 text-white">Your account</H1>
 
 			<div className="h-[600px]">
-				<ContentBlock className="h-[500px] flex justify-center items-center">
+				<ContentBlock className="h-[500px] flex flex-col justify-center items-center gap-3 ">
 					<p>Logged in as {session.user.email}</p>
+					<SignOutBtn />
 				</ContentBlock>
 			</div>
 		</main>
