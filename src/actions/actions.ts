d3a439b25fbb25f415c0d803db23f3c62675e203
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-10 17:07:51 +0200

    Add a loading state to the auth form.

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -13,6 +13,8 @@ import { redirect } from "next/navigation";
 // User actions
 
 export async function logIn(formData: unknown) {
+	await sleep(1000);
+	
 	if (!(formData instanceof FormData)) {
 		return {
 			message: "Invalid form data",
@@ -23,6 +25,8 @@ export async function logIn(formData: unknown) {
 }
 
 export async function signUp(formData: unknown) {
+	await sleep(1000);
+
 	// check if formData is a FormData type
 	if (!(formData instanceof FormData)) {
 		return {
