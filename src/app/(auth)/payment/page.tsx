c927c61f1d8e90077e7b46a947a6568ca29d0eb5
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-11 17:37:07 +0200

    Payment successful&cancel message after the payment based on SearchParams logic

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -4,16 +4,33 @@ import { createCheckoutSession } from "@/actions/actions";
 import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
 
-export default function Page() {
+export default function Page({ searchParams }) {
 	return (
 		<main className="flex flex-col items-center space-y-10">
-			<H1> Petsoft access requires payment</H1>
-			<Button
-				onClick={async () => {
-					await createCheckoutSession();
-				}}>
-				But lifetime access for 299$
-			</Button>
+			<H1> Zoofy access requires payment</H1>
+
+			{!searchParams.success && (
+				<Button
+					onClick={async () => {
+						await createCheckoutSession();
+					}}>
+					But lifetime access for 299$
+				</Button>
+			)}
+
+			{searchParams.success && (
+				<div className="flex flex-col items-center space-y-5 text-green-700">
+					<h1 className="text-xl font-bold">Payment successful!</h1>
+					<p className="text-lg">You now have lifetime access to Zoofy.</p>
+				</div>
+			)}
+
+			{searchParams.cancel && (
+				<div className="flex flex-col items-center space-y-5 text-red-700">
+					<h1 className="text-xl font-bold">Payment cancelled</h1>
+					<p className="text-lg">You can try again if you want.</p>
+				</div>
+			)}
 		</main>
 	);
 }
