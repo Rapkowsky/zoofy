10b683ae900a29b162130e5c726e579af6f7e383
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-11 17:43:49 +0200

    Add Loading State using (useTransition) for Server Action outside a form

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -3,16 +3,21 @@
 import { createCheckoutSession } from "@/actions/actions";
 import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
 
 export default function Page({ searchParams }) {
+	const [isPending, startTransition] = useTransition();
 	return (
 		<main className="flex flex-col items-center space-y-10">
 			<H1> Zoofy access requires payment</H1>
 
 			{!searchParams.success && (
 				<Button
+					disabled={isPending}
 					onClick={async () => {
-						await createCheckoutSession();
+						startTransition(async () => {
+							await createCheckoutSession();
+						});
 					}}>
 					But lifetime access for 299$
 				</Button>
@@ -25,7 +30,7 @@ export default function Page({ searchParams }) {
 				</div>
 			)}
 
-			{searchParams.cancel && (
+			{searchParams.cancelled && (
 				<div className="flex flex-col items-center space-y-5 text-red-700">
 					<h1 className="text-xl font-bold">Payment cancelled</h1>
 					<p className="text-lg">You can try again if you want.</p>
