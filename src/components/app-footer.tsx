430a0ce80ff56fa273a778188728400ef1b50b8a
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-13 15:49:22 +0200

    Display current year

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,8 +1,10 @@
 export default function AppFooter() {
+	const date = new Date();
+    const year = date.getFullYear();
 	return (
 		<footer className="mt-auto border-t border-black/5 py-5">
 			<small className="opacity-50">
-				&copy; 2030 Robert Rapkowski. All rights reserved.
+				&copy; {year} Robert Rapkowski. All rights reserved.
 			</small>
 		</footer>
 	);
