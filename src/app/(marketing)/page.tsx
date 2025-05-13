987f56b09238667928e1b457508f17dffe79488a
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-13 17:44:10 +0200

    Fix marketing page responsivenes on mobile + create separate component for preview img

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,24 +1,25 @@
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
+import ZoofyPreview from "@/components/zoofy-preview";
 
 export default function Home() {
 	return (
-		<main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
-			<Image
-				src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
-				alt="Preview of Zoofy"
-				width={519}
-				height={472}
-			/>
+		<main className="bg-[#5DC9A8] min-h-screen flex flex-col md:flex-row items-center justify-center gap-10 p-5">
+			<div className="md:block hidden max-w-[500px] max-[1024px]:flex-1">
+				<ZoofyPreview />
+			</div>
 
-			<div>
+			<div className="max-[1024px]:flex-1">
 				<Logo />
 				<h1 className="text-5xl font-semibold my-6 max-w-[500px]">
 					Manage your <span className="font-extrabold">pet daycare</span> with
 					ease
 				</h1>
+
+				<div className="mb-6 md:hidden max-w-[500px] w-full">
+					<ZoofyPreview />
+				</div>
 				<p className="text-2xl font-medium max-w-[600px]">
 					Use Zoofy to easily keep track of pets under your care. Get lifetime
 					access for $299.
---
+++ src/components/zoofy-preview.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import zoofyPreview from "../../public/zoofy-preview-marketing.png";
+import Image from "next/image";
+
+export default function ZoofyPreview() {
+	return <Image src={zoofyPreview} alt="Preview of Zoofy" />;
+}
